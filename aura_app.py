da661b8f83506afd69891590031eadd0ec60afff
kareemrogers-coder <kareemrogers27@gmail.com>
2024-12-14 21:33:52 -0500

    update

--- aura_app.py
+++ aura_app.py
@@ -6,7 +6,9 @@ app = create_app('DevelopmentConfig')
 
 
 
-
-with app.app_context():
-    #db.drop_all()
-    db.create_all()
+if __name__ == '__main__':
+    with app.app_context():
+        #db.drop_all()
+        db.create_all()
+    
+    app.run(debug=True)
